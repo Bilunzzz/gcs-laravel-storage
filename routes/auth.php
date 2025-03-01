798ce60f532b8c339a3c36073bd92f51cf8b5a80
Bilunzzz <hainakamakmur06@gmail.com>
2025-03-01 22:12:06 +0700

    Add admin middleware to protect admin dashboard route in auth.php

--- routes/auth.php
+++ routes/auth.php
@@ -57,3 +57,7 @@ Route::middleware('auth')->group(function () {
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
 });
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/admin/dashboard', [AdminController::class, 'index']);
+});
